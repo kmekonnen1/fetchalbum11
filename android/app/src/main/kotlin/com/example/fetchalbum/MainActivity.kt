e73b061408da64e3e8d9c1ccd5c1414965ac5a29
kidanu.t@gmail.com <kidanu.t@gmail.com>
2023-07-10 15:59:49 -0400

    My updates

---
+++ android/app/src/main/kotlin/com/example/fetchalbum/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fetchalbum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/fetch_album_test.dart
@@ -0,0 +1,68 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'fetch_album_test.mocks.dart';
+import 'package:fetchalbum/main.dart';
+
+
+
+// Generate a MockClient using the Mockito package.
+// Create new instances of this class in each test.
+
+@GenerateMocks([http.Client])
+void main() {
+  group('fetchAlbum', () {
+    test('returns an Album if the http call completes successfully', () async {
+      final client = MockClient();
+
+      // Use Mockito to return a successful response when it calls the
+      // provided http.Client.
+      when(client
+              .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
+          .thenAnswer((_) async =>
+              http.Response('{"userId": 1, "id": 2, "title": "mock"}', 200));
+
+      expect(await fetchAlbum(client), isA<Album>());
+    });
+    test('throws an exception if the http call completes with an error', () {
+      final client = MockClient();
+
+      // Use Mockito to return an unsuccessful response when it calls the
+      // provided http.Client.
+      when(client
+              .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(fetchAlbum(client), throwsException);
+    });
+    //When the code is mistyped, the test fails, code 300 in stead of 200
+      test('Test fails, returns an error if wrong code is entered, say 300', () async {
+      final client = MockClient();
+
+      // Use Mockito to return a successful response when it calls the
+      // provided http.Client.
+      when(client
+              .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
+          .thenAnswer((_) async =>
+              http.Response('{"userId": 1, "id": 2, "title": "mock"}', 300));
+
+      expect(await fetchAlbum(client), isA<Album>());
+    });
+    //When wrong data is provided, test fails, 'kidanu' for the title instead of 'mock'
+       test('Test fails, returns an error if the wrong value is entered for the object', () async {
+      final client = MockClient();
+
+      // Use Mockito to return a successful response when it calls the
+      // provided http.Client.
+      when(client
+              .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
+          .thenAnswer((_) async =>
+              http.Response('{"userId": 1, "id": 2, "title": "kidanu"}', 200));
+
+      expect(await fetchAlbum(client), isA<Album>());
+    });
+  });
+}
